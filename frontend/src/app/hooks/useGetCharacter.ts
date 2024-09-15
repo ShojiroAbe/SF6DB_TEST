84d9747441c19f3f88c621b75a1c54da6c97b31e
ShojiroAbe <astook89@gmail.com>
2024-09-16 01:22:28 +0900

    環境変数を追加

--- frontend/src/app/hooks/useGetCharacter.ts
+++ frontend/src/app/hooks/useGetCharacter.ts
@@ -1,15 +1,12 @@
 import useSWR from 'swr'
 
-// 環境変数行き予定
-const API_URL = 'http://localhost:8000'
-
 async function fetcher(key: string) {
   // keyにはエンドポイントが入ります
   return fetch(key).then((res) => res.json())
 }
 
 export const useGetCharacter = () => {
-  const { data, error, isLoading } = useSWR(`${API_URL}/get/character`, fetcher)
+  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_LOCAL_API_URL}/get/character`, fetcher)
 
   console.log('data', data);
   console.log('error', error);
